3be66a476fdbab2744095172e31370cd97b13935
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-10 07:49:35 +0100

    Adding Canvas Course

---
+++ Courses/Canvas API/main.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("our-canvas");
+
+let context = canvas.getContext("2d");
+
+// Videos [01 -- 06]
+
+// let gradient = context.createLinearGradient(0, 0, 200, 0);
+
+// gradient.addColorStop(0, "red");
+// gradient.addColorStop(1, "blue");
+
+// context.strokeStyle = gradient;
+
+// context.font = "32px Tahoma";
+// context.strokeRect(20, 20, 200, 100);
+// context.strokeText("hello", 100, 170);
+
+// Videos [06 -- 10]
