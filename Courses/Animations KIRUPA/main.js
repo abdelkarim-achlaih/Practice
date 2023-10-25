cb86e403e72910443963f071e1156073bb2d7021
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-26 00:51:34 +0100

    Slide and Bounce an Image on Hover

--- Courses/Animations KIRUPA/main.js
+++ Courses/Animations KIRUPA/main.js
@@ -1,2 +1,20 @@
 // ------------------------------------------- Animating Drop Shadows ------------------------
 
+// ------------------------------------------- Slide and Bounce an Image on Hover ------------------------
+
+let picContainers = document.querySelectorAll(".picture-container img");
+
+picContainers.forEach((container) => {
+	container.addEventListener("mouseover", hoverOn, false);
+	container.addEventListener("mouseout", hoverOff, false);
+});
+function hoverOn(e) {
+	e.target.classList.remove("slideOut");
+	e.target.classList.add("slideIn");
+	e.target.style.top = "0";
+}
+function hoverOff(e) {
+	e.target.classList.remove("slideIn");
+	e.target.classList.add("slideOut");
+	e.target.style.top = "-150px";
+}
