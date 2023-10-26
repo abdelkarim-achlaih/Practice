f01138f9b3c8845be253a60265eae73e537caaa8
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-26 19:38:16 +0100

    Animating Items Around a Point -- Start

--- Courses/Animations KIRUPA/main.js
+++ Courses/Animations KIRUPA/main.js
@@ -2,19 +2,21 @@
 
 // ------------------------------------------- Slide and Bounce an Image on Hover ------------------------
 
-let picContainers = document.querySelectorAll(".picture-container img");
+// let picContainers = document.querySelectorAll(".picture-container img");
 
-picContainers.forEach((container) => {
-	container.addEventListener("mouseover", hoverOn, false);
-	container.addEventListener("mouseout", hoverOff, false);
-});
-function hoverOn(e) {
-	e.target.style.top = getComputedStyle(e.target).top;
-	e.target.classList.remove("slideOut");
-	e.target.classList.add("slideIn");
-}
-function hoverOff(e) {
-	e.target.style.top = getComputedStyle(e.target).top;
-	e.target.classList.remove("slideIn");
-	e.target.classList.add("slideOut");
-}
+// picContainers.forEach((container) => {
+// 	container.addEventListener("mouseover", hoverOn, false);
+// 	container.addEventListener("mouseout", hoverOff, false);
+// });
+// function hoverOn(e) {
+// 	e.target.style.top = getComputedStyle(e.target).top;
+// 	e.target.classList.remove("slideOut");
+// 	e.target.classList.add("slideIn");
+// }
+// function hoverOff(e) {
+// 	e.target.style.top = getComputedStyle(e.target).top;
+// 	e.target.classList.remove("slideIn");
+// 	e.target.classList.add("slideOut");
+// }
+
+// ------------------------------------------- Animating Items Around a Point ------------------------
