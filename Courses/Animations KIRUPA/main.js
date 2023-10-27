4e6a1381c1f35633dd32e4f1b24c8cff1734f715
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-27 10:26:40 +0100

    Sliding / Scrolling Animation with Infinite Items

--- Courses/Animations KIRUPA/main.js
+++ Courses/Animations KIRUPA/main.js
@@ -20,3 +20,35 @@
 // }
 
 // ------------------------------------------- Animating Items Around a Point ------------------------
+
+// ------------------------------------------- Sliding / Scrolling Animation with Infinite Items ------------------------
+
+let container = document.querySelector(".container");
+let items = document.querySelectorAll(".item");
+
+items.forEach((item) => {
+	item.data = {
+		one: getRandomNumber(0, 10),
+		two: getRandomNumber(0, 10),
+	};
+	setContent(item);
+});
+
+container.addEventListener("animationiteration", updateAndGenerate, false);
+
+function updateAndGenerate(e) {
+	e.target.data = {
+		one: e.target.data.two,
+		two: getRandomNumber(0, 10),
+	};
+
+	setContent(e.target);
+}
+
+function getRandomNumber(n, m) {
+	return Math.floor(Math.random() * (m - n + 1)) + n;
+}
+function setContent(item) {
+	item.children[0].innerHTML = item.data.one;
+	item.children[1].innerHTML = item.data.two;
+}
