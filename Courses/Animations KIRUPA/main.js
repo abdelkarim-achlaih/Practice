c823b1026013701bece623fe122b0918ee6d1e67
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-01 09:03:04 +0100

    The Falling Snow Effect Last modif

--- Courses/Animations KIRUPA/main.js
+++ Courses/Animations KIRUPA/main.js
@@ -123,10 +123,10 @@
 
 let container = document.querySelector(".container");
 
-// for (let i = 0; i < 1; i++) {
-// 	let newSnowflake = container.firstElementChild.cloneNode();
-// 	container.append(newSnowflake);
-// }
+for (let i = 0; i < 20; i++) {
+	let newSnowflake = container.firstElementChild.cloneNode();
+	container.append(newSnowflake);
+}
 
 let snowFlakes = container.querySelectorAll("img");
 
@@ -136,7 +136,6 @@ function initialStyles() {
 	snowFlakes.forEach((snowFlake) => {
 		snowFlake.styles = {};
 		randomStyles(snowFlake);
-		snowFlake.styles.y = -51;
 		changeStyles(snowFlake);
 	});
 }
@@ -149,7 +148,7 @@ function randomStyles(snowFlake) {
 
 function randomCoordinates(snowFlake) {
 	snowFlake.styles.x = getRandomNumber(0, 450);
-	snowFlake.styles.y = getRandomNumber(0, 400);
+	snowFlake.styles.y = -51;
 }
 
 function changeStyles(snowFlake) {
@@ -162,12 +161,15 @@ function getRandomNumber(n, m) {
 	return Math.floor(Math.random() * (m - n + 1)) + n;
 }
 
+function randomDestinationCoordinates(snowFlake) {
+	snowFlake.styles.destinationX = getRandomNumber(0, 500);
+	snowFlake.styles.destinationY = 501;
+}
+
 let speed = 0;
 let direction = 0;
-let delta = { x: 0, y: 0 };
-let tmp = {};
 
-// window.addEventListener("load", setup, false);
+window.addEventListener("load", setup, false);
 
 function setup() {
 	snowFlakes.forEach((snowFlake) => {
@@ -176,45 +178,31 @@ function setup() {
 		function animate(snowFlake) {
 			randomDestinationCoordinates(snowFlake);
 
-			// speed = getRandomNumber(1, 2);
-			// direction = [1, -1][getRandomNumber(0, 1)];
-			// snowFlake.styles.accelAmount = speed * direction;
-
-			snowFlake.styles.oldX = snowFlake.styles.x;
-			snowFlake.styles.oldY = snowFlake.styles.y;
-
-			tmp.x = snowFlake.styles.destinationX - snowFlake.styles.x;
-			tmp.y = snowFlake.styles.destinationY - snowFlake.styles.y;
-			snowFlake.styles.deltaDest = Math.sqrt(tmp.x * tmp.x + tmp.y * tmp.y);
+			snowFlake.styles.speed = 5 + 40 * Math.random();
+			snowFlake.styles.direction = Math.random() < 0.5 ? 1 : -1;
+			snowFlake.styles.counter = 0;
 
 			move();
 
 			function move() {
-				delta.x = snowFlake.styles.x - snowFlake.styles.oldX;
-				delta.y = snowFlake.styles.y - snowFlake.styles.oldY;
-				snowFlake.styles.deltaCurr = Math.sqrt(
-					delta.x * delta.x + delta.y * delta.y
-				);
-				console.log(snowFlake.styles);
-				if (snowFlake.styles.deltaCurr < snowFlake.styles.deltaDest) {
-					console.log("hi");
-					snowFlake.styles.x += 1;
-					snowFlake.styles.y += 1;
+				if (snowFlake.styles.y < container.clientHeight + 1) {
+					snowFlake.styles.counter += snowFlake.styles.speed / 5000;
+					snowFlake.styles.x +=
+						(snowFlake.styles.direction *
+							snowFlake.styles.speed *
+							Math.cos(snowFlake.styles.counter)) /
+						40;
+					snowFlake.styles.y +=
+						Math.sin(snowFlake.styles.counter) / 40 +
+						snowFlake.styles.speed / 30;
 					changeStyles(snowFlake);
 					requestAnimationFrame(move);
 				} else {
 					randomCoordinates(snowFlake);
-					snowFlake.styles.y = -51;
 					changeStyles(snowFlake);
 					animate(snowFlake);
 				}
 			}
 		}
 	});
-}
-
-function randomDestinationCoordinates(snowFlake) {
-	snowFlake.styles.destinationX = getRandomNumber(0, 500);
-	// snowFlake.styles.destinationY = getRandomNumber(550, 560);
-	snowFlake.styles.destinationY = 501;
 }
