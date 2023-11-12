4b9e62c08aef8f5f3d63c97bcb320959aae81eab
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-12 18:12:49 +0100

    Quiz app || App Logic || write bullets func

--- Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/3- Apps/3- Quiz App/js/main.js
+++ Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/3- Apps/3- Quiz App/js/main.js
@@ -18,15 +18,28 @@ async function loadQuests() {
 }
 loadQuests();
 function setup(data) {
-	// cat.innerHTML = "HTML";
-	// count.innerHTML = data.length;
-	// let quest = randomQuests(data);
-	// title.innerHTML = quest.title;
-	// writeQuest(quest);
-	console.log(randomAllQuests(data));
+	data = randomAllQuests(data);
+	cat.innerHTML = "HTML";
+	count.innerHTML = data.length;
+	let quest = data[0];
+	writeQuest(quest);
+	writeBullets(data, "setup");
 }
-
+function writeBullets(data, when) {
+	if (when === "setup") {
+		for (let i = 0; i < data.length; i++) {
+			let span = document.createElement("span");
+			if (i === 0) {
+				span.classList.add("on");
+			}
+			bulletsSpans.append(span);
+		}
+	}
+	if (when === "update") {
+	}
+}
 function writeQuest(quest) {
+	title.innerHTML = quest.title;
 	let keys = Object.keys(quest);
 	for (let i = 1; i < keys.length; i++) {
 		let answer = document.createElement("div");
@@ -38,12 +51,11 @@ function writeQuest(quest) {
 	}
 }
 
-
-function randomQuests(data) {
-	let randomObj = data[getRandomNumber(0, data.length)];
-	console.log(randomObj);
-	return randomObj;
-}
+// function randomQuests(data) {
+// 	let randomObj = data[getRandomNumber(0, data.length)];
+// 	console.log(randomObj);
+// 	return randomObj;
+// }
 
 function randomAllQuests(data) {
 	let tmpData = data;
@@ -51,19 +63,69 @@ function randomAllQuests(data) {
 	while (output.length < tmpData.length) {
 		let tmpIndex = getRandomNumber(0, tmpData.length - 1);
 		if (!output.includes(tmpData[tmpIndex])) {
-
+			randomAnswers(tmpData[tmpIndex]);
 			output.push(tmpData[tmpIndex]);
 		}
 	}
 	return output;
 }
-function randomAnswers(quest) {
-	let tmpIndex = getRandomNumber(1, Object.keys(quest).length - 1);
-  
-	return randomObj;
-}
+// function randomAnswers(quest) {
+// 	let tmpIndex = getRandomNumber(1, Object.keys(quest).length - 1);
+// 	console.log(tmpIndex);
+// 	let output = {};
+// 	let tmpKeys = [];
+// 	if (!tmpKeys.includes(quest[tmpIndex])) {
+// 		tmpKeys.push(quest[tmpIndex]);
+// 		let tmp = tmpKeys[tmpKeys.length - 1];
+// 		output[tmp] = quest[tmpIndex];
+// 	}
+// 	console.log(output);
+// 	// return randomObj;
+// }
 function getRandomNumber(n, m) {
 	let tmp = Math.floor(Math.random() * (m - n + 1)) + n;
-	console.log(tmp);
 	return tmp;
 }
+
+let questions = {
+	title: "In HTML, which tag is used to define the document type?",
+	right_answer: "&lt;!DOCTYPE html&gt;",
+	answer_1: "&lt;doctype&gt;",
+	answer_2: "&lt;type&gt;",
+	answer_3: "&lt;doc&gt;",
+	answer_4: "&lt;!DOCTYPE&gt;",
+};
+
+// function randomAnswers(questions) {
+// 	let tmpKeys = Object.keys(questions);
+// 	let tmpValues = Object.values(questions);
+// 	let tmpLength = tmpKeys.length;
+// 	console.log("object length", tmpLength);
+// 	console.log("Init output");
+// 	let output = {};
+// 	console.log(output);
+// 	console.log(Object.keys(output).length);
+// 	// while (Object.keys(output).length < tmpKeys.length) {
+// 	for (let i = 0; i < 10; i++) {
+// 		let tmpIndex = getRandomNumber(1, tmpLength - 1);
+// 		console.log("random number", tmpIndex);
+// 		if (!output.hasOwnProperty(tmpKeys[tmpIndex])) {
+// 			console.log("nope");
+// 			let key = tmpKeys[tmpIndex];
+// 			let value = questions[key];
+// 			output[key] = value;
+// 		}
+// 		console.log("Iteration output");
+// 		console.log(output);
+// 	}
+// 	output = Object.assign(output, {
+// 		"tmpKeys[0]": tmpValues[0],
+// 		"tmpKeys[1]": tmpValues[1],
+// 	});
+
+// 	console.log("final output");
+// 	console.log(output);
+// }
+// randomAnswers(questions);
+
+function randomAnswers(question) {}
