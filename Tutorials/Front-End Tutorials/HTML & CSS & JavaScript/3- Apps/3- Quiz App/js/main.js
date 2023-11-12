708be16e1055a64376fc44ef3347d019a0345187
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-12 18:30:43 +0100

    Quiz app || App Logic || write answers with random func

--- Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/3- Apps/3- Quiz App/js/main.js
+++ Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/3- Apps/3- Quiz App/js/main.js
@@ -41,91 +41,36 @@ function writeBullets(data, when) {
 function writeQuest(quest) {
 	title.innerHTML = quest.title;
 	let keys = Object.keys(quest);
-	for (let i = 1; i < keys.length; i++) {
-		let answer = document.createElement("div");
-		answer.innerHTML = `
-        <input type="radio" name="answers" id="answer_${i}">
-        <label for="answer_${i}">${quest[keys[i]]}</label>`;
-		answer.classList.add("answer");
-		answersArea.append(answer);
+	let writtenKeys = [];
+	while (writtenKeys.length < keys.length - 1) {
+		let index = getRandomNumber(1, keys.length - 1);
+		if (!writtenKeys.includes(index)) {
+			writtenKeys.push(index);
+			writeAnswer(quest, index);
+		}
 	}
 }
-
-// function randomQuests(data) {
-// 	let randomObj = data[getRandomNumber(0, data.length)];
-// 	console.log(randomObj);
-// 	return randomObj;
-// }
-
 function randomAllQuests(data) {
 	let tmpData = data;
 	let output = [];
 	while (output.length < tmpData.length) {
 		let tmpIndex = getRandomNumber(0, tmpData.length - 1);
 		if (!output.includes(tmpData[tmpIndex])) {
-			randomAnswers(tmpData[tmpIndex]);
 			output.push(tmpData[tmpIndex]);
 		}
 	}
 	return output;
 }
-// function randomAnswers(quest) {
-// 	let tmpIndex = getRandomNumber(1, Object.keys(quest).length - 1);
-// 	console.log(tmpIndex);
-// 	let output = {};
-// 	let tmpKeys = [];
-// 	if (!tmpKeys.includes(quest[tmpIndex])) {
-// 		tmpKeys.push(quest[tmpIndex]);
-// 		let tmp = tmpKeys[tmpKeys.length - 1];
-// 		output[tmp] = quest[tmpIndex];
-// 	}
-// 	console.log(output);
-// 	// return randomObj;
-// }
 function getRandomNumber(n, m) {
 	let tmp = Math.floor(Math.random() * (m - n + 1)) + n;
 	return tmp;
 }
-
-let questions = {
-	title: "In HTML, which tag is used to define the document type?",
-	right_answer: "&lt;!DOCTYPE html&gt;",
-	answer_1: "&lt;doctype&gt;",
-	answer_2: "&lt;type&gt;",
-	answer_3: "&lt;doc&gt;",
-	answer_4: "&lt;!DOCTYPE&gt;",
-};
-
-// function randomAnswers(questions) {
-// 	let tmpKeys = Object.keys(questions);
-// 	let tmpValues = Object.values(questions);
-// 	let tmpLength = tmpKeys.length;
-// 	console.log("object length", tmpLength);
-// 	console.log("Init output");
-// 	let output = {};
-// 	console.log(output);
-// 	console.log(Object.keys(output).length);
-// 	// while (Object.keys(output).length < tmpKeys.length) {
-// 	for (let i = 0; i < 10; i++) {
-// 		let tmpIndex = getRandomNumber(1, tmpLength - 1);
-// 		console.log("random number", tmpIndex);
-// 		if (!output.hasOwnProperty(tmpKeys[tmpIndex])) {
-// 			console.log("nope");
-// 			let key = tmpKeys[tmpIndex];
-// 			let value = questions[key];
-// 			output[key] = value;
-// 		}
-// 		console.log("Iteration output");
-// 		console.log(output);
-// 	}
-// 	output = Object.assign(output, {
-// 		"tmpKeys[0]": tmpValues[0],
-// 		"tmpKeys[1]": tmpValues[1],
-// 	});
-
-// 	console.log("final output");
-// 	console.log(output);
-// }
-// randomAnswers(questions);
-
-function randomAnswers(question) {}
+function writeAnswer(quest, index) {
+	let keys = Object.keys(quest);
+	let answer = document.createElement("div");
+	answer.innerHTML = `
+					<input type="radio" name="answers" id="answer_${index}">
+					<label for="answer_${index}">${quest[keys[index]]}</label>`;
+	answer.classList.add("answer");
+	answersArea.append(answer);
+}
