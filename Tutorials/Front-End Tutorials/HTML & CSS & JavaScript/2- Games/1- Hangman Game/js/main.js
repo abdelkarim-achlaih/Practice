0cf3465d8c7e8af90ade14f02849cf6491739c0e
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-18 09:43:47 +0100

    Hangman Game || Game Logic || Check true

--- Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/2- Games/1- Hangman Game/js/main.js
+++ Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/2- Games/1- Hangman Game/js/main.js
@@ -1,10 +1,11 @@
 let lettersDiv = document.querySelector(".letters");
-const letters = "abcdefghijklmnopqrseuvwxyz";
+const letters = "abcdefghijklmnopqrstuvwxyz";
 let alphabets = letters.split("");
 alphabets.forEach((alpha) => {
 	let div = document.createElement("div");
 	div.classList.add("letter-box");
 	div.innerText = alpha;
+	div.addEventListener("click", check, false);
 	lettersDiv.appendChild(div);
 });
 
@@ -44,8 +45,39 @@ function getRandomNumber(n, m) {
 	return tmp;
 }
 
-let cat = document.querySelector(".category span");
 let wordsKeys = Object.keys(words);
 let randomKey = wordsKeys[getRandomNumber(0, wordsKeys.length - 1)];
 let word = words[randomKey][getRandomNumber(0, words[randomKey].length - 1)];
-cat.innerText = word;
+document.querySelector(".category span").innerText = word;
+
+let letterGuess = document.querySelector(".letters-guess");
+Array.from(word).forEach((letter) => {
+	let span = document.createElement("span");
+	letterGuess.append(span);
+});
+let correct = 0;
+function check(e) {
+	let ele = e.target.innerText.toLowerCase();
+	word = word.toLowerCase();
+	if (correct < word.length) {
+		if (word.includes(ele)) {
+			let indexes = getAllIndexesOf(word, ele);
+			indexes.forEach((index) => {
+				letterGuess.querySelector(`span:nth-child(${index + 1})`).innerText =
+					ele;
+				correct++;
+			});
+			displayPopup();
+		} else {
+			console.log("no");
+		}
+	}
+}
+function getAllIndexesOf(str, search) {
+	let tmp = [];
+	Array.from(str).forEach((letter, index) => {
+		letter === search ? tmp.push(index) : "";
+	});
+	return tmp;
+}
+function displayPopup() {}
