2ac60ccfcac936af77a275a1ded6146569cf5b8f
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-12-05 22:18:33 +0100

    Catch Selectors

--- Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/2- Games/2 -Typing Speed Test Game/js/main.js
+++ Tutorials/Front-End Tutorials/HTML & CSS & JavaScript/2- Games/2 -Typing Speed Test Game/js/main.js
@@ -30,3 +30,17 @@ const words = [
 	"Rust",
 	"Playing",
 ];
+
+let time = 5;
+
+// Catch Selectors
+let startButton = document.querySelector(".start");
+let lvlNameSpan = document.querySelector(".message .lvl");
+let secondsSpan = document.querySelector(".message .seconds");
+let theWord = document.querySelector(".the-word");
+let upcomingWords = document.querySelector(".upcoming-words");
+let input = document.querySelector(".input");
+let timeLeftSpan = document.querySelector(".time span");
+let scoreGot = document.querySelector(".score .got");
+let scoreTotal = document.querySelector(".score .total");
+let finishMessage = document.querySelector(".finish");
