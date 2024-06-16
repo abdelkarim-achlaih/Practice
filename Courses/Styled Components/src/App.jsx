c504ebbb6ab3697d5244b0dcd6d540e858653dd5
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-06-16 15:19:04 +0100

    Adding the code base

---
+++ Courses/Styled Components/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function App() {
+	return (
+		<>
+			<h1>Progress Tracker</h1>
+			<div>
+				<div className="section">
+					<div className="weekday-title">M</div>
+				</div>
+				<div className="section">
+					<div className="weekday-title">T</div>
+				</div>
+				<div className="section">
+					<div className="weekday-title">W</div>
+				</div>
+				<div className="section">
+					<div className="weekday-title">T</div>
+				</div>
+				<div className="section">
+					<div className="weekday-title">F</div>
+				</div>
+				<div className="section">
+					<div className="weekday-title">S</div>
+				</div>
+				<div className="section">
+					<div className="weekday-title">S</div>
+				</div>
+			</div>
+		</>
+	);
+}
